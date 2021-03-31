ff83b485a02c75b4e5d4ffc9c7e2710ba04233fe
johnson <phantruongson96@gmail.com>
2021-03-30 23:03:25 -0400

    maps project

---
+++ maps/src/index.ts
@@ -0,0 +1,7 @@
+import { User } from './User';
+import { Company } from './Company';
+
+// Google api key: AIzaSyD4JUUFnIFIZ5mgcF5uWs5XncIxnQiVg6g 
+const user = new User();
+const company = new Company();
+console.log(user, company);
